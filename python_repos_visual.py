f2fcdc11b44ddaf9dfb54b53f80b4067e20ab2a2
Muhammed Shittu <devshittu@gmail.com>
2023-04-18 17:04:55 +0100

    Adding Custom Tooltips

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -18,15 +18,18 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 repo_dicts = response_dict["items"]
 print(f"Repositories returned: {len(repo_dicts)}")
 
-repo_names, stars = [], []
+repo_names, stars, hover_texts = [], [], []
 
 # To summarize the top repos in a file.
 print("\nFind the summary below")
 for repo_dict in repo_dicts:
-    print("\nSelected information about repository:")
     repo_names.append(repo_dict["name"])
     stars.append(repo_dict["stargazers_count"])
-    print("\n\n")
+    # Build hover texts
+    owner = repo_dict["owner"]["login"]
+    description = repo_dict["description"]
+    hover_text = f"{owner} <br/> {description}"
+    hover_texts.append(hover_text)
 
 # Make visualizations.
 
@@ -34,6 +37,6 @@ title = "Most-Starred Python Projects on GitHub"
 
 lables = {'x': 'Repository', 'y': 'Stars'}
 
-fig = px.bar(x=repo_names, y=stars, title=title, labels=lables)
+fig = px.bar(x=repo_names, y=stars, title=title, labels=lables, hover_name=hover_texts)
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20,)
 fig.show()
