df13363586f926d0b9b9354b733bf35fd6673379
Muhammed Shittu <devshittu@gmail.com>
2023-04-18 16:20:54 +0100

    Removed redundant data

--- python_repos.py
+++ python_repos.py
@@ -20,18 +20,6 @@ print(f"Repositories returned: {len(repo_dicts)}")
 # Examine the first repo
 repo_dict = repo_dicts[0]
 print(f"\nKeys: {len(repo_dict)}")
-# for key in sorted(repo_dict.keys()):
-#     print(f"{key}")
-    
-# print(f"\nFind the summary below")
-# print("\nSelected information about first repository:")
-# print(f"Name: {repo_dict['name']}")
-# print(f"Owner: {repo_dict['owner']['login']}")
-# print(f"Stars: {repo_dict['stargazers_count']}")
-# print(f"Repository: {repo_dict['html_url']}")
-# print(f"Created: {repo_dict['created_at']}")
-# print(f"Updated: {repo_dict['updated_at']}")
-# print(f"Description: {repo_dict['description']}")
 
 # To summarize the top repos in a file.
 index = 0
