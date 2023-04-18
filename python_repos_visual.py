4b7e4dc8d68cfaa684d0042fb15e7ec34d7a275a
Muhammed Shittu <devshittu@gmail.com>
2023-04-18 17:06:52 +0100

    Adding Custom Tooltips fixed the br in the hover_text

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -28,7 +28,7 @@ for repo_dict in repo_dicts:
     # Build hover texts
     owner = repo_dict["owner"]["login"]
     description = repo_dict["description"]
-    hover_text = f"{owner} <br/> {description}"
+    hover_text = f"{owner} <br /> {description}"
     hover_texts.append(hover_text)
 
 # Make visualizations.
