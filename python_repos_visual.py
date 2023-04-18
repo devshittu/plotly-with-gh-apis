e2539b0742b3b6ad3ace9e10e7f0b08950b8d033
Muhammed Shittu <devshittu@gmail.com>
2023-04-18 16:22:48 +0100

    Started visualization of repos

---
+++ python_repos_visual.py
@@ -0,0 +1,38 @@
+import requests
+
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r=requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+response_dict = r.json()
+# print(response_dict.keys())
+
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+# Explore information about the repositories
+repo_dicts = response_dict["items"]
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repo
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+
+# To summarize the top repos in a file.
+index = 0
+print(f"\nFind the summary below")
+for repo_dict in repo_dicts[0:]:
+    index +=1
+    print(f"\nSelected information about {index} repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    print("\n\n")
+
