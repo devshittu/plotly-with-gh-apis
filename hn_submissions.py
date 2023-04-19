ac29041e28c4462c6aa829d172d8bc393ba576fb
Muhammed Shittu <devshittu@gmail.com>
2023-04-19 19:01:02 +0100

    Add the bar graph bar to the repos.

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,9 +1,10 @@
 from operator import itemgetter
-import requests
 import json
+import requests
+import plotly.express as px
 
 url="https://hacker-news.firebaseio.com/v0/topstories.json"
-r=requests.get(url)
+r=requests.get(url, timeout=3600)
 print(f"Status code: {r.status_code}")
 
 submission_ids = r.json()
@@ -11,11 +12,10 @@ submission_dicts = []
 
 for submission_id in submission_ids[:5]:
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
-    r = requests.get(url)
+    r = requests.get(url, timeout=3600)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
-    
-    # Build a dict for earch article
+    # Build a dict for each article
     submission_dict = {
         'title': response_dict["title"],
         'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
@@ -24,11 +24,18 @@ for submission_id in submission_ids[:5]:
     submission_dicts.append(submission_dict)
     
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
-
+submission_comment_counts, submission_links = [], [] 
 for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
     print(f"Discussion link: {submission_dict['hn_link']}")
     print(f"Comments: {submission_dict['comments']}")
+    submission_comment_counts.append(submission_dict['comments'])
+    submission_links.append(submission_dict['title'])
     
 response_string = json.dumps(submission_dicts, indent=4)
 print(response_string)
+
+labels =  {'x': 'Aritcles', 'y': 'Comments'}
+fig = px.bar(x=submission_comment_counts, y=submission_links, labels=labels)
+# fig.update_layout()
+fig.show()
